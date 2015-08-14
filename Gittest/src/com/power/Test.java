3a3a11e393a37dd432f1dd6967daafe51e68fd55
zhangjunwei <zhangjunw007@Gmail.com>
2015-08-14 18:39:57 +0800

    the first

---
+++ Gittest/src/com/power/Test.java
@@ -0,0 +1,10 @@
+package com.power;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("this the first git test");
+	}
+
+}
