198c9581b232e1d2349114b6da5a1654140e2f67
zhangjunwei <zhangjunw007@Gmail.com>
2015-08-14 18:49:13 +0800

    123456

---
+++ gitTesttwo/src/com/power/test.java
@@ -0,0 +1,11 @@
+package com.power;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("this the first git test");
+          System.out.println("this the second git test");
+	}
+
+}
